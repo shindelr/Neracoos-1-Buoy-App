b708a45eb5c05c9c7aec1dc00f16b432e910a962
shindelr <129821483+shindelr@users.noreply.github.com>
2026-04-24 13:44:36 -0700

    Moved latest conditions to @bottom

--- app/(platformMap)/@bottom/platform/[platformId]/current_conditions.tsx
+++ app/(platformMap)/@bottom/platform/[platformId]/current_conditions.tsx
@@ -1,11 +1,36 @@
 "use client"
+import { Row, Col } from "react-bootstrap"
+
 import { UsePlatform } from "Features/ERDDAP/hooks"
+import { PlatformFeature } from "Features/ERDDAP/types"
 import { ErddapCurrentPlatformConditions } from "Features/ERDDAP/Platform/Observations/CurrentConditions"
+import { ErddapObservationTable } from "Features/ERDDAP/Platform/Observations/Table/table"
+import { UnitSelector, useUnitSystem } from "Features/Units"
+import { aDayAgoRounded } from "Shared/time"
 
 export function CurrentConditions({ platformId }: { platformId: string }) {
+  const unitSystem = useUnitSystem()
+  const aDayAgo = aDayAgoRounded()
+
   return (
     <UsePlatform platformId={platformId}>
-      {({ platform }) => <ErddapCurrentPlatformConditions platform={platform} />}
+      {({ platform }: { platform: PlatformFeature }) => {
+        return (
+          <Row>
+            <Col xs={12} md={8}>
+              <ErddapCurrentPlatformConditions platform={platform} />
+            </Col>
+            <Col xs={12} md={4}>
+              <ErddapObservationTable
+                platform={platform}
+                unitSelector={<UnitSelector />}
+                unitSystem={unitSystem}
+                laterThan={aDayAgo}
+              />
+            </Col>
+          </Row>
+        )
+      }}
     </UsePlatform>
   )
 }
--- src/components/PlatformInfo/platformInfo.tsx
+++ src/components/PlatformInfo/platformInfo.tsx
@@ -3,31 +3,18 @@ import React from "react"
 
 import { PlatformAlerts } from "Features/ERDDAP/Platform/Alerts"
 import { ErddapPlatformInfoPanel } from "Features/ERDDAP/Platform/Info"
-import { ErddapObservationTable } from "Features/ERDDAP/Platform/Observations/Table/table"
 import { UsePlatform } from "Features/ERDDAP/hooks/BuoyBarnComponents"
-
-import { UnitSelector, useUnitSystem } from "Features/Units"
-import { aDayAgoRounded } from "Shared/time"
 
 /**
  * Display our platform info panel for the select platform.
  */
 export const PlatformInfo = ({ id }: { id: string }) => {
-  const unitSystem = useUnitSystem()
-  const aDayAgo = aDayAgoRounded()
-
   return (
     <UsePlatform platformId={id}>
       {({ platform }) => (
         <React.Fragment>
           <PlatformAlerts platform={platform} />
           <ErddapPlatformInfoPanel platform={platform} />
-          <ErddapObservationTable
-            platform={platform}
-            unitSelector={<UnitSelector />}
-            unitSystem={unitSystem}
-            laterThan={aDayAgo}
-          />
         </React.Fragment>
       )}
     </UsePlatform>
